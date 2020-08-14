57e8eec7daf30dc5ad8fb7733bebd41c3e9b06e8
silverbeen <kub0803@naver.com>
2020-08-14 10:13:38 +0900

    facebook

--- facebook.js
+++ facebook.js
@@ -1,6 +1,26 @@
-var str = document.getElementById("textarea").value;
+const face={
+    maked:document.getElementById("think"),
+    add:document.getElementById("modal_make"),
+};
 
-str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+var openmodal = document.getElementById("openmodal");
+openmodal.addEventListener("click",()=> {
+    var modal = document.getElementById("modal");
+    modal.style.display = "flex";
+});
 
-document.getElementById("text").innerHTML = str;
+face.add.addEventListener("click",() => {
 
+    var str = document.getElementById("modal_feed").value;
+    str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    const facetemplate = `<div>${str}</div>`;
+    
+});
+
+var Delate=document.getElementById("modal_img");
+Delate.addEventListener("click", () => {
+    modal.style.display="none";
+});
+
+var change=document.getElementById("modal_make");
+change.d
