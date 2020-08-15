60b655435fc7b16b40ec5c872a330912b4ad871f
silverbeen <kub0803@naver.com>
2020-08-15 23:51:41 +0900

    facebook

--- facebook.js
+++ facebook.js
@@ -1,26 +1,61 @@
 const face={
     maked:document.getElementById("think"),
     add:document.getElementById("modal_make"),
-};
+    list: document.getElementById("main_feed"),
+    have:document.getElementById("main_center"),
 
-var openmodal = document.getElementById("openmodal");
+};
+let openmodal = document.getElementById("openmodal");
+
 openmodal.addEventListener("click",()=> {
     var modal = document.getElementById("modal");
     modal.style.display = "flex";
 });
 
+
+
 face.add.addEventListener("click",() => {
 
     var str = document.getElementById("modal_feed").value;
     str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    const facetemplate = `<div>${str}</div>`;
+
+    const faceTemplate = `<div>${str}</div>`;
+
+    const listTemplate=`<li id="main_feed">
+    <div id="main_feed-top">
+        <div id="main_feed-name">
+            <img id="main_feed-face" src="./main-profile.png" alt="">
+                <div id="main_feed-time">
+                <span>은빈</span>
+            </div>
+        </div>
+        <img src="./modal-icon-more.png" alt="">
+    </div>
+    <span id="Text">
+        ${str}</span>
+    <hr class="bor">
+    <div id="main_feed-bottom">
+        <div id="item">
+            <img src="./good.png" alt="">
+            <span>좋아요</span>
+        </div>
+        <div id="item">
+            <img src="./comment.png" alt="">
+            <span>댓글 달기</span>
+        </div>
+        <div id="item">
+            <img src="./share.png" alt="">
+            <span>공유하기</span>
+        </div>
+    </div>
+</li>`
+
+
+face.have.insertAdjacentHTML("beforeend", listTemplate);
     
 });
 
-var Delate=document.getElementById("modal_img");
+let Delate=document.getElementById("modal_img");
 Delate.addEventListener("click", () => {
     modal.style.display="none";
 });
-
-var change=document.getElementById("modal_make");
-change.d
